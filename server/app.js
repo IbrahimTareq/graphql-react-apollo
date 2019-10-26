b7de302e60d7a1732d323e325badd9067439a300
Ibrahim Tareq <ibrahim.tareq@news.com.au>
2019-10-26 11:37:31 +1100

    Added graphql schema (WIP)

--- server/app.js
+++ server/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
-
-const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
+
+// bind express with graphql
+app.use('/graphql', graphqlHTTP({
+    schema: schema
+}));
 
 app.listen(4000, () => {
     console.log('now listening for requests on port 4000');
